a7049a836ea439ea35fcb3d8be177c028788123b
Gabriel Dewana Prasetyo <dewanaprasetyo411@gmail.com>
2024-04-26 16:42:42 +0700

    Membuat tutor pages

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,33 @@
-import "./new.scss"
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import List from "../../components/datatable/Datatable";
 
 const New = () => {
-    return (
-        <div>New</div>
-    )
-}
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+        <h1 className="title">New</h1>
+        </div>
+        <div className="bottom">
+            <div className="left">
+                <div className="item">
+                <img
+                    src="https://i.pinimg.com/564x/59/27/3f/59273fd2918b801b34d69fd03d42a03f.jpg"
+                    alt=""
+                    className="itemImg"/>
+                </div>
+            </div>
+            <div className="right">
+                <List/>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default New;
