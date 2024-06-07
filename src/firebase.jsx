03dee167b6daeca04320a0597a91023f86bd5f0a
Gabriel Dewana Prasetyo <dewanaprasetyo411@gmail.com>
2024-06-07 16:32:05 +0700

    firebase done

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "store-tutorial-f0cd6.firebaseapp.com",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:432568521554:web:1787e8ebea4d064e79208b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,19 @@
 import "./login.scss"; 
-import { useState } from "react";
+import { useContext,useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => { 
   console.log(process.env.REACT_APP_FIREBASE_KEY);
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
+
+  const {dispatch} = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -16,6 +22,8 @@ const Login = () => {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user; 
+        dispatch({type:"LOGIN", payload:user});
+        navigate("/");
         console.log(user);
       })
       .catch((error) => {
