d896ad58b174d9d00cdc72d690c9e4a04e3c75ea
Gabriel Dewana Prasetyo <dewanaprasetyo411@gmail.com>
2024-06-07 15:05:05 +0700

    firebase

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,46 @@
-import "./login.scss";
+import "./login.scss"; 
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = () => { 
+  console.log(process.env.REACT_APP_FIREBASE_KEY);
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user; 
+        console.log(user);
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email" 
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password" 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
